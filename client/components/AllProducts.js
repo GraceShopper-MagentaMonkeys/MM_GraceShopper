8c25e015592a06a80b93f85850aa139ade7b2a8d
Brendan Shulman <brendan.shulman@gmail.com>
2021-11-09 12:52:05 -0500

    fixed bugs and added updates

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -4,33 +4,40 @@ import { connect } from 'react-redux';
 class AllProducts extends React.Component {
   constructor() {
     super();
-    this.state = [
-      {
-        img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
-        name: 'Gold Cactus',
-        price: 6012,
-        description: 'this is a perfectly golden cactus. Please buy it',
-      },
-      {
-        img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
-        name: 'Whiskey Shaving Cream for Women',
-        price: 30,
-        description: 'Idk buy it if you want?',
-      },
-      {
-        img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
-        name: 'Rocky Mountain fertility Medicine',
-        price: 500,
-        description: 'If ya know ya know',
-      },
-    ];
+    this.state = {
+      products: [
+        {
+          img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
+          name: 'Gold Cactus',
+          price: 6012,
+          description: 'this is a perfectly golden cactus. Please buy it',
+        },
+        {
+          img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
+          name: 'Whiskey Shaving Cream for Women',
+          price: 30,
+          description: 'Idk buy it if you want?',
+        },
+        {
+          img: 'https://wisdmlabs.com/site/wp-content/uploads/2016/04/inventory.png',
+          name: 'Rocky Mountain fertility Medicine',
+          price: 500,
+          description: 'If ya know ya know',
+        },
+      ],
+    };
   }
   render() {
     return (
       <div>
-        {this.state.map((product) => {
+        {this.state.products.map((product) => {
           return (
-            <div key={product.name}>
+            <div
+              key={product.name}
+              onClick={() => {
+                console.log('You clicked me');
+              }}
+            >
               <img src={product.img} />
               <h4>
                 {product.name} - ${product.price}
