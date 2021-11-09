1425ae0e0c0e0d2692ed051d55361430f4211c5d
Jordan Laguio <jordan.laguio@gmail.com>
2021-11-09 12:05:10 -0500

    sequelize models completed

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,15 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
 
 //associations could go here!
+User.belongsToMany(Product,{ through: 'cart'})
+Product.belongsToMany(User, { through: 'cart'})
 
 module.exports = {
   db,
   models: {
-    User,
+    User, Product
   },
 }
---
+++ server/db/models/Product.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+// product name, product price, description, quantity INT, category
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DECIMAL(10,2),
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      defaultValue: 1,
+      allowNull: false
+    }
+  },
+  category: {
+    type: Sequelize.ENUM(['food/drink', 'style', 'home']),
+    validate: {
+      defaultValue: 'home'
+    }
+  }
+})
+
+
+module.exports = Product
