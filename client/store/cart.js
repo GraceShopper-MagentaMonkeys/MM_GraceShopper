cb778d23707256ba7c0d901766e8a0149473f4c9
Jordan Oetting <joetting71@gmail.com>
2021-11-10 19:33:57 +0000

    updating

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,3 +1,17 @@
 import axios from 'axios';
 
+const GET_SELECTED_PRODUCTS = 'GET_SELECTED_PRODUCTS'
+
 //ACTION CREATOR for getting selected products
+const selectedProducts = products => {
+    return {
+        type: GET_SELECTED_PRODUCTS,
+        products
+    }
+}
+
+const initialState = [];
+
+export default function cartReducer(state = initialState, action) {
+    
+}
