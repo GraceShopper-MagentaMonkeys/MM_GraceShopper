3cb1c9ce42fd4d4d416428190677484e9e0da4e0
Jordan Oetting <joetting71@gmail.com>
2021-11-11 15:48:26 +0000

    css changes from code review

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,3 +1,33 @@
 import axios from 'axios';
 
+const GET_SELECTED_PRODUCTS = 'GET_SELECTED_PRODUCTS'
+
 //ACTION CREATOR for getting selected products
+const selectedProducts = products => {
+    return {
+        type: GET_SELECTED_PRODUCTS,
+        products
+    }
+}
+
+export const getSelectedProducts = (userId) => {
+    return async dispatch => {
+        try{
+            const { data } = await axios.get(`/cart/${userId}`);
+            dispatch(selectedProducts(data));
+        } catch (e){
+            console.log('Sorry could not find any products in your cart', e)
+        }
+    }
+}
+
+const initialState = [];
+
+export default function cartReducer (state = initialState, action) {
+    switch (action.type){
+        case GET_SELECTED_PRODUCTS: 
+            return action.products ;
+        default:
+            return state ;
+    }
+}
