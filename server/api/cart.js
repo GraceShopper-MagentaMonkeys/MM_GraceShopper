f26d482da690a2731e907b3ba6cb88db79128b64
Brendan Shulman <brendan.shulman@gmail.com>
2021-11-12 14:53:13 -0500

    changes for jordan

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,34 +1,17 @@
 const router = require('express').Router();
 const {
-  models: { User, Product },
+  models: { User, Product, Cart },
 } = require('../db');
 module.exports = router;
 
 // mounted in /api/cart
 
-// need to define cart model?
-// const Cart = db.define('cart', {
-//   itemId: Sequelize.INTEGER,
-//   references: {
-//     model: User,
-//     key: 'id'
-//   }
-// })
-
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const userCart = User.findAll({
-//       include: {
-//         model: User,
-//         as: 'cart',
-//         where: {
-//           userId: req.params.id,
-//         },
-//       },
-//     });
-
-//     res.json(userCart);
-//   } catch (e) {
-//     next(e);
-//   }
-// });
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    const userCart = await user.getProducts();
+    res.json(userCart);
+  } catch (e) {
+    next(e);
+  }
+});
