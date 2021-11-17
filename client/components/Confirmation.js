45b1c26e7375f8051b2176d3720a6babe3e72781
Brendan Shulman <brendan.shulman@gmail.com>
2021-11-17 12:23:58 -0500

    added checkout and confirmation components

---
+++ client/components/Confirmation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Confirmation = () => {
+  let nums = Math.floor(Math.random() * 2000000000);
+  return (
+    <div>
+      <h1>Thank You</h1>
+      <h4>{`Order confirmation #${nums}`}</h4>
+      <Link to={'/allproducts'}>
+        <button>Continue Shopping</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Confirmation;
